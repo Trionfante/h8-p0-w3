83686df7a45fa64c8e02652af612d76aa6241991
Trionfante <melv_hartan@live.com>
2019-08-04 09:36:52 +0700

    upload exercise 2: reverse string

---
+++ exercise-2.js
@@ -0,0 +1,9 @@
+function balikString(kata) {
+    var reverse ='';
+    for (let i = kata.length-1; i >= 0; i--){
+        reverse = reverse + kata[i];
+    }
+    return reverse;
+}
+
+console.log(balikString('hello world!'));
