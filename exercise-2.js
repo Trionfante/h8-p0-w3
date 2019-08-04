2bfb05882370e8d63b8243fda0e536e144c06966
Trionfante <melv_hartan@live.com>
2019-08-04 09:39:46 +0700

    reverse string

--- exercise-2.js
+++ exercise-2.js
@@ -1,9 +1,13 @@
-function balikString(kata) {
-    var reverse ='';
-    for (let i = kata.length-1; i >= 0; i--){
-        reverse = reverse + kata[i];
-    }
-    return reverse;
-}
+// Reverse String Without .reverse Method
 
-console.log(balikString('hello world!'));
+    function balikString(kata) {
+        var reverse ='';
+        for (let i = kata.length-1; i >= 0; i--){
+            reverse = reverse + kata[i];
+        }
+        return reverse;
+    }
+
+    console.log(balikString('hello world!'));
+
+// End of Reverse String
